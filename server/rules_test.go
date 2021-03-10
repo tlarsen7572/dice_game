9a5ce6dc885a460b92b4654778356f23b84a78a8
Tom Larsen <larsenthomasj@gmail.com>
2021-03-10 06:08:19 -0500

    rename test

--- server/rules_test.go
+++ server/rules_test.go
@@ -3,7 +3,7 @@ package server_test
 import "testing"
 import "server"
 
-func TestRolledDice(t *testing.T) {
+func TestRollDice(t *testing.T) {
 	roll := server.RollDice(6)
 	if actualCount := len(roll); actualCount != 6 {
 		t.Fatalf(`expected 6 dice but got %v`, actualCount)
