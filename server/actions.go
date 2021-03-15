74f5dfa7c9819c0383d849c093410832a2b4bf7f
Tom Larsen <larsenthomasj@gmail.com>
2021-03-15 10:00:01 -0400

    refactor generating new game action

--- server/actions.go
+++ server/actions.go
@@ -1,12 +1,71 @@
 package server
 
-type Action struct {
-	Name     string
-	Function func() map[int]Action
+import (
+	"fmt"
+	"math/rand"
+	"strings"
+	"time"
+)
+
+func NewGameManager(baseUrl string) *GameManager {
+	baseUrl = strings.TrimSuffix(baseUrl, `/`)
+	manager := &GameManager{
+		ActiveGame:    nil,
+		ActiveActions: Actions{},
+		ActionLinks:   map[int]ActionInfo{},
+		randGenerator: rand.New(rand.NewSource(time.Now().UTC().UnixNano())),
+		baseUrl:       baseUrl,
+	}
+	manager.generateActions()
+	return manager
 }
 
-type ActionFunc func() map[int]Action
+type ActionType int
 
-func GenerateActions(game *Game) map[int]Action {
-	return nil
+const (
+	NewGameAction ActionType = 0
+	RollAction    ActionType = 1
+	NewTurnAction ActionType = 2
+)
+
+type ActionInfo struct {
+	Type   ActionType
+	Url    string
+	Method string
+}
+
+type Actions struct {
+	NewGameAction func(int)
+	RollAction    func()
+	NewTurnAction func()
+}
+
+type GameManager struct {
+	ActiveGame    *Game
+	ActiveActions Actions `json:"-"`
+	ActionLinks   map[int]ActionInfo
+	randGenerator *rand.Rand
+	baseUrl       string
+}
+
+func (m *GameManager) generateActions() {
+	m.ActionLinks = map[int]ActionInfo{}
+	m.ActiveActions = Actions{}
+	if m.ActiveGame == nil {
+		m.generateNewGameAction()
+		return
+	}
+}
+
+func (m *GameManager) generateNewGameAction() {
+	id := m.randGenerator.Int()
+	m.ActiveActions.NewGameAction = func(winningScore int) {
+		m.ActiveGame = NewGame(winningScore)
+		m.generateActions()
+	}
+	m.ActionLinks[id] = ActionInfo{
+		Type:   NewGameAction,
+		Url:    fmt.Sprintf(`%v/%v?winningScore={WinningScore}`, m.baseUrl, id),
+		Method: "POST",
+	}
 }
--- server/actions_test.go
+++ server/actions_test.go
@@ -1,12 +1,54 @@
 package server_test
 
 import (
+	"encoding/json"
+	"regexp"
 	"server"
 	"testing"
 )
 
-func TestNewGameActions(t *testing.T) {
-	game := server.NewGame(10000)
-	actions := server.GenerateActions(game)
-	print(actions)
+func TestNewGameManager(t *testing.T) {
+	gameManager := server.NewGameManager("http://localhost")
+	if gameManager.ActiveActions.NewGameAction == nil {
+		t.Fatalf(`expected non-nil new game action but got nil`)
+	}
+	if gameManager.ActiveActions.RollAction != nil {
+		t.Fatalf(`expected nil roll action but got non-nil`)
+	}
+	if gameManager.ActiveActions.NewTurnAction != nil {
+		t.Fatalf(`expected nil turn action but got non-nil`)
+	}
+	if actions := len(gameManager.ActionLinks); actions != 1 {
+		t.Fatalf(`expected 1 action but got %v`, actions)
+	}
+	for _, action := range gameManager.ActionLinks {
+		if action.Type != server.NewGameAction {
+			t.Fatalf(`expected ActionType %v but got %v`, server.NewGameAction, action.Type)
+		}
+		if action.Method != `POST` {
+			t.Fatalf(`expected POST but got %v`, action.Method)
+		}
+		expectedUrl := `http://localhost/[0-9]+\?winningScore=\{WinningScore\}`
+		if matches, _ := regexp.MatchString(expectedUrl, action.Url); !matches {
+			t.Fatalf(`expected '%v' but got '%v'`, expectedUrl, action.Url)
+		}
+		t.Logf(`url: %v`, action.Url)
+	}
+	jsonBytes, _ := json.Marshal(gameManager)
+	t.Logf(string(jsonBytes))
+}
+
+func TestGenerateNewGame(t *testing.T) {
+	t.Skip(`skipping to do some refactoring`)
+	gameManager := server.NewGameManager(`http://localhost`)
+	gameManager.ActiveActions.NewGameAction(10000)
+	if gameManager.ActiveGame == nil {
+		t.Fatalf(`expected non-nil game but got nil`)
+	}
+	if gameManager.ActiveActions.NewGameAction == nil {
+		t.Fatalf(`expected non-nil new game action but got nil`)
+	}
+	if gameManager.ActiveActions.NewTurnAction == nil {
+		t.Fatalf(`expected non-nil new game action but got nil`)
+	}
 }
