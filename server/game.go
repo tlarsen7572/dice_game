cd940e23ff5b9571c4d51eddbd988ad009ab4071
Tom Larsen <larsenthomasj@gmail.com>
2021-03-14 21:20:38 -0400

    re-do game logic

--- server/game.go
+++ server/game.go
@@ -1,94 +1,40 @@
 package server
 
-type Roller interface {
-	Roll(int) []int
-}
-
-type options struct {
-	overrideRollWith Roller
-}
-
-type OptionSetter func(*options) *options
-
-func OverrideRollWith(roller Roller) OptionSetter {
-	return func(o *options) *options {
-		o.overrideRollWith = roller
-		return o
-	}
-}
-
-func NewGame(winningScore int, optionSetters ...OptionSetter) *Game {
-	o := &options{}
-	for _, setter := range optionSetters {
-		o = setter(o)
-	}
-
-	g := &Game{}
-	if o.overrideRollWith != nil {
-		g.roller = o.overrideRollWith.Roll
-	} else {
-		g.roller = RollDice
-	}
-
-	g.winningScore = winningScore
-	g.Roll = g.generateRollAction(6)
-	return g
-}
-
-type RollAndScore struct {
-	Roll        []int
-	Score       int
-	ScoringDice []int
-}
+import (
+	"encoding/json"
+)
 
 type Game struct {
-	winningScore     int
-	scoredTurns      []int
-	currentTurnScore int
-	lastRoll         []int
-	lastScore        ScoredRoll
-	roller           func(int) []int
-
-	Roll    func()
-	NewTurn func()
+	WinningScore int
+	CurrentScore int
+	Roller       func(int) []int `json:"-"`
+	Turns        []int
+	ActiveTurn   *Turn
 }
 
-func (g *Game) NewGame() {
-
+func (g *Game) NewTurn() {
+	g.CurrentScore += g.ActiveTurn.Score
+	g.Turns = append(g.Turns, g.ActiveTurn.Score)
+	g.ActiveTurn.Reset()
 }
 
-func (g *Game) newTurn() {
-
+func (g *Game) Roll() {
+	g.createTurnIfNil()
+	diceToRoll := 6 - len(g.ActiveTurn.LastRoll)
+	roll := g.Roller(diceToRoll)
+	g.ActiveTurn.Roll(roll)
 }
 
-func (g *Game) LastRolledScore() RollAndScore {
-	return RollAndScore{
-		Roll:        g.lastRoll,
-		Score:       g.lastScore.Score,
-		ScoringDice: g.lastScore.ScoringDice,
+func (g *Game) createTurnIfNil() {
+	if g.ActiveTurn == nil {
+		g.ActiveTurn = &Turn{}
 	}
 }
 
-func (g *Game) CurrentTurnScore() int {
-	return g.currentTurnScore
-}
-
-func (g *Game) generateRollAction(totalDice int) func() {
-	return func() {
-		g.lastRoll = g.roller(totalDice)
-		g.lastScore = Score(g.lastRoll)
-		g.currentTurnScore += g.lastScore.Score
-		if g.lastScore.Score == 0 {
-			g.currentTurnScore = 0
-			g.Roll = nil
-			return
-		}
-
-		g.NewTurn = g.newTurn
-		diceRemaining := totalDice - len(g.lastScore.ScoringDice)
-		if diceRemaining == 0 {
-			diceRemaining = 6
-		}
-		g.Roll = g.generateRollAction(diceRemaining)
+func (g *Game) ToJson() string {
+	jsonBytes, err := json.Marshal(g)
+	if err != nil {
+		return err.Error()
 	}
+	return string(jsonBytes)
 }
--- server/game_test.go
+++ server/game_test.go
@@ -5,16 +5,6 @@ import (
 	"testing"
 )
 
-func TestStartGame(t *testing.T) {
-	game := server.NewGame(10000)
-	if game.Roll == nil {
-		t.Fatalf(`expected non-nil Roll action but got nil`)
-	}
-	if game.NewTurn != nil {
-		t.Fatalf(`expected nil NewTurn action but got non-nil`)
-	}
-}
-
 type MockRoller struct {
 	RollOverride  []int
 	RequestedDice int
@@ -28,57 +18,43 @@ func (r *MockRoller) Roll(totalDice int) []int {
 	return r.RollOverride
 }
 
-func TestTurnEndingWithZeroPoints(t *testing.T) {
-	mockRoll := &MockRoller{}
-	game := server.NewGame(10000, server.OverrideRollWith(mockRoll))
-	mockRoll.RollOverride = []int{1, 2, 2, 3, 3, 4}
+func TestGameFirstScoringTurn(t *testing.T) {
+	mockRoller := &MockRoller{
+		RollOverride: []int{1, 2, 2, 3, 3, 4},
+	}
+	game := &server.Game{
+		WinningScore: 10000,
+		Roller:       mockRoller.Roll,
+	}
 	game.Roll()
-	score := game.LastRolledScore()
-	if mockRoll.RequestedDice != 6 {
-		t.Fatalf(`expected 6 requested dice but got %v`, mockRoll.RequestedDice)
+	if game.CurrentScore != 0 {
+		t.Fatalf(`expected 0 but got %v`, game.CurrentScore)
 	}
-	if score.Score != 100 {
-		t.Fatalf(`expected score of 100 but got %v`, score.Score)
+	game.NewTurn()
+	if game.CurrentScore != 100 {
+		t.Fatalf(`expected 100 but got %v`, game.CurrentScore)
 	}
-	if game.CurrentTurnScore() != 100 {
-		t.Fatalf(`expected 100 but got %v`, game.CurrentTurnScore())
+	if len(game.Turns) != 1 {
+		t.Fatalf(`expected 1 turn but got %v`, len(game.Turns))
 	}
-	if game.Roll == nil {
-		t.Fatalf(`expected nil Roll action but got non-nil`)
+}
+
+func TestGameToJson(t *testing.T) {
+	game := &server.Game{
+		WinningScore: 10000,
+		CurrentScore: 500,
+		Roller:       server.RollDice,
+		Turns:        []int{100, 200, 0, 200},
+		ActiveTurn: &server.Turn{
+			Score:           100,
+			LastRoll:        []int{1, 2, 2, 3, 3, 4},
+			LastScoringDice: []int{0},
+		},
 	}
 
-	mockRoll.RollOverride = []int{1, 2, 3, 3, 4}
-	game.Roll()
-	score = game.LastRolledScore()
-	if mockRoll.RequestedDice != 5 {
-		t.Fatalf(`expected 5 requested dice but got %v`, mockRoll.RequestedDice)
-	}
-	if score.Score != 100 {
-		t.Fatalf(`expected score of 100 but got %v`, score.Score)
-	}
-	if game.CurrentTurnScore() != 200 {
-		t.Fatalf(`expected 200 but got %v`, game.CurrentTurnScore())
-	}
-	if game.Roll == nil {
-		t.Fatalf(`expected nil Roll action but got non-nil`)
-	}
-
-	mockRoll.RollOverride = []int{2, 3, 3, 4}
-	game.Roll()
-	score = game.LastRolledScore()
-	if mockRoll.RequestedDice != 4 {
-		t.Fatalf(`expected 4 requested dice but got %v`, mockRoll.RequestedDice)
-	}
-	if score.Score != 0 {
-		t.Fatalf(`expected score of 0 but got %v`, score.Score)
-	}
-	if game.CurrentTurnScore() != 0 {
-		t.Fatalf(`expected 0 but got %v`, game.CurrentTurnScore())
-	}
-	if game.Roll != nil {
-		t.Fatalf(`expected non-nil Roll action but got nil`)
-	}
-	if game.NewTurn == nil {
-		t.Fatalf(`expected non-nil NewTurn action but got nil`)
+	actualJsonStr := game.ToJson()
+	expectedJsonStr := `{"WinningScore":10000,"CurrentScore":500,"Turns":[100,200,0,200],"ActiveTurn":{"Score":100,"LastRoll":[1,2,2,3,3,4],"LastScoringDice":[0]}}`
+	if actualJsonStr != expectedJsonStr {
+		t.Fatalf("expected\n'%v'\nbut got\n'%v'", expectedJsonStr, actualJsonStr)
 	}
 }
