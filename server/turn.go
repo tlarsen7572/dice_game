cf4d4295bce2d1bd82781183714ff4b74c615bec
Tom Larsen <larsenthomasj@gmail.com>
2021-03-14 20:48:02 -0400

    define turn logic separately

---
+++ server/turn.go
@@ -0,0 +1,23 @@
+package server
+
+type Turn struct {
+	Score           int
+	LastRoll        []int
+	LastScoringDice []int
+}
+
+func (t *Turn) Roll(roll []int) {
+	t.LastRoll = roll
+	score := Score(roll)
+	t.LastScoringDice = score.ScoringDice
+	t.Score += score.Score
+	if score.Score == 0 {
+		t.Score = 0
+	}
+}
+
+func (t *Turn) Reset() {
+	t.Score = 0
+	t.LastScoringDice = []int{}
+	t.LastRoll = []int{}
+}
---
+++ server/turn_test.go
@@ -0,0 +1,45 @@
+package server_test
+
+import (
+	"reflect"
+	"server"
+	"testing"
+)
+
+func TestNormalTurn(t *testing.T) {
+	turn := &server.Turn{}
+	turn.Roll([]int{1, 2, 2, 3, 3, 4})
+	if turn.Score != 100 {
+		t.Fatalf(`expected 100 but got %v`, turn.Score)
+	}
+	if !reflect.DeepEqual(turn.LastRoll, []int{1, 2, 2, 3, 3, 4}) {
+		t.Fatalf(`expected %v but got %v`, []int{1, 2, 2, 3, 3, 4}, turn.LastRoll)
+	}
+	if !reflect.DeepEqual(turn.LastScoringDice, []int{0}) {
+		t.Fatalf(`expected %v but got %v`, []int{0}, turn.LastScoringDice)
+	}
+}
+
+func TestScoringZeroReducesTotalScoreToZero(t *testing.T) {
+	turn := &server.Turn{}
+	turn.Roll([]int{1, 2, 2, 3, 3, 4})
+	turn.Roll([]int{2, 2, 3, 3, 4})
+	if turn.Score != 0 {
+		t.Fatalf(`expected 0 but got %v`, turn.Score)
+	}
+}
+
+func TestResetTurn(t *testing.T) {
+	turn := &server.Turn{}
+	turn.Roll([]int{1, 2, 2, 3, 3, 4})
+	turn.Reset()
+	if turn.Score != 0 {
+		t.Fatalf(`expected 0 but got %v`, turn.Score)
+	}
+	if !reflect.DeepEqual(turn.LastScoringDice, []int{}) {
+		t.Fatalf(`expected an empty slice but got %v`, turn.LastScoringDice)
+	}
+	if !reflect.DeepEqual(turn.LastRoll, []int{}) {
+		t.Fatalf(`expected an empty slice but got %v`, turn.LastRoll)
+	}
+}
